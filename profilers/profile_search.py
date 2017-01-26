e3ce43bca7c85c14008fe4f9343bf295527052ec
Greg Lever <greglever@gmail.com>
2017-01-26 12:52:37 +0000

    Ideas for pigeonhole principle approach

--- profilers/profile_search.py
+++ profilers/profile_search.py
@@ -11,13 +11,24 @@ from .boyer_moore import string_search
 
 
 def profile_genome_search():
-    """Profile the sort bucket step"""
+    """
+    Profile the sort bucket step
+    
+    The idea is to use the "pidgeonhole principle" which will allow us
+    to  use the Boyer-Moore algorithm to find approximate (as opposed to exact) matches by 
+    dividing the pattern P up into partitions, looking for each of 
+    those partitions using an exact matching algorithm (Boyer-Moore). And then, 
+    wherever we find an occurrence of one of those partitions, 
+    we can do a verification step where we look in the neighborhood to 
+    see if we see a full occurrence of P with up to the maximum number 
+    of differences allowed. 
+    """
     genome_file = 'Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz'
     pattern = b'TGGATGTGAAATGAGTCAAG'
     testText = b'CGCTAAAAGCTAGAGCTACGCGACGATCAGCACTACGTGGATGTGAAATGAGTCAAGCGCGCTAGACGACTACGACTAGCAGCATCGATCGATCGATCG'
     maxmismatch = 3
     string_search(P=pattern, t=testText)
-    # TODO Your implementation
+    # TODO: (Greg) Implementation
 
 
 def main():
